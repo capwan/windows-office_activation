e51f04958e6e1780193dc7d79331bb59af1f9990
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-06-02 22:09:54 +0400

    latest version

---
+++ office16or19.ps1
@@ -0,0 +1,9 @@
+﻿cd 'C:\Program Files\Microsoft Office\Office16\'
+cscript.exe .\OSPP.VBS ## [ospp.htm -- справка] 
+cscript.exe .\OSPP.VBS /dstatus
+
+## MAK-ключ и активируем :
+
+cscript.exe OSPP.VBS /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+
+cscript.exe OSPP.VBS /act
