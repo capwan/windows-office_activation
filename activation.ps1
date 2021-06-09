2af3efe40ebda81340ede0cefc5236b77a92c3ae
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-06-09 14:27:19 +0400

    changed comments

--- activation.ps1
+++ activation.ps1
@@ -1,3 +1,3 @@
-﻿slmgr /skms kms.loli.best #[Выдаём значение "kms.loli.best"]
-slmgr /ato #[Процесс активации идёт]
-slmgr /xpr #[Для проверки результата]
+﻿slmgr /skms kms.loli.best #[We give value "kms.loli.best"]
+slmgr /ato #[Activation process]
+slmgr /xpr #[For checkup result]
